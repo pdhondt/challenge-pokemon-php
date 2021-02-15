189cd91958474f208860dce9ac625d4ce12b6818
Peter <dhondt.peter@gmail.com>
2021-02-15 14:30:06 +0100

    added moves, images, prev/next pokemon, prev evolution

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 if (isset($_GET['text-num'])) {
     $pok_api_link = 'https://pokeapi.co/api/v2/pokemon/';
     $pok_api = $pok_api_link . $_GET['text-num'];
@@ -10,6 +11,21 @@ if (isset($_GET['text-num'])) {
     $pok_img = $pok_array['sprites']['front_default'];
     $pok_weight = $pok_array['weight'];
     $pok_height = $pok_array['height'];
+    $pok_moves = array_slice($pok_array['moves'], 0, 4);
+
+    $species_api = $pok_array['species']['url'];
+    $species_json = file_get_contents($species_api);
+    $species_array = json_decode($species_json, true);
+    $evolves_from = $species_array['evolves_from_species'];
+    if (!empty($evolves_from)) {
+        $evolves_from_name = $evolves_from['name'];
+        $evolves_from_api = $pok_api_link . $evolves_from_name;
+        $evolves_from_array = json_decode(file_get_contents($evolves_from_api), true);
+        $evolves_from_image = $evolves_from_array['sprites']['front_default'];
+        $evolves_from_id = $evolves_from_array['id'];
+    }
+
+
 
 }
 
@@ -36,39 +52,39 @@ if (isset($_GET['text-num'])) {
     <div class="main-container" id="main-container">
         <div class="main-screen" id="main-screen">
             <div class="front-sprite" id="front-sprite">
-                <img id="poke-sprite" src="<?php echo $pok_img; ?>" alt="woops">
-                <p id="poke-name"><?php echo $pok_name; ?></p>
-                <p id="poke-id"><?php echo $pok_id; ?></p>
+                <img id="poke-sprite" src="<?php echo $pok_img ?>" alt="woops">
+                <p id="poke-name"><?php echo $pok_name ?></p>
+                <p id="poke-id"><?php echo $pok_id ?></p>
             </div>
-            <div class="weight" id="weight"><?php echo $pok_weight; ?></div>
-            <div class="height" id="height"><?php echo $pok_height; ?></div>
+            <div class="weight" id="weight"><?php echo $pok_weight ?></div>
+            <div class="height" id="height"><?php echo $pok_height ?></div>
         </div>
         <div class="type-screen" id="type-screen">
             <div class="type1-s" id="type1-s"></div>
             <div class="type2-s" id="type2-s"></div>
         </div>
         <div class="buttons" id="buttons">
-            <div class="btn-up" id="btn-up" onclick="nextEvolution()"></div>
-            <div class="btn-r" id="btn-r" onclick="nextPokemon()"></div>
-            <div class="btn-down" id="btn-down" onclick="previousEvolution()"></div>
-            <div class="btn-l" id="btn-l" onclick="previousPokemon()"></div>
+            <a href="" class="btn-up" id="btn-up">Next evolution</a>
+            <a href="index.php?text-num=<?php echo $pok_id + 1 ?>" class="btn-r" id="btn-r">Next pokémon</a>
+            <a href="index.php?text-num=<?php echo $evolves_from_id ?>" class="btn-down" id="btn-down">Previous evolution</a>
+            <a href="index.php?text-num=<?php echo $_GET['text-num'] - 1 ?>" class="btn-l" id="btn-l">Previous pokémon</a>
         </div>
     </div>
     <div class="right-container" id="right-container">
         <div class="move-screen" id="move-screen">
-            <div class="move1" id="move1"><p class="moves">1</p></div>
-            <div class="move2" id="move2"><p class="moves">2</p></div>
-            <div class="move3" id="move3"><p class="moves">3</p></div>
-            <div class="move4" id="move4"><p class="moves">4</p></div>
+            <div class="move1" id="move1"><p class="moves"><?php echo $pok_moves[0]['move']['name'] ?></p></div>
+            <div class="move2" id="move2"><p class="moves"><?php echo $pok_moves[1]['move']['name'] ?></p></div>
+            <div class="move3" id="move3"><p class="moves"><?php echo $pok_moves[2]['move']['name'] ?></p></div>
+            <div class="move4" id="move4"><p class="moves"><?php echo $pok_moves[3]['move']['name'] ?></p></div>
         </div>
         <div class="evolutions" id="evolutions">
-            <img src="" alt="" id="img-1">
-            <img src="" alt="" id="img-2">
+            <img src="<?php echo $evolves_from_image ?>" alt="" id="img-1">
+            <img src="<?php echo $pok_img ?>" alt="" id="img-2">
             <img src="" alt="" id="img-3">
         </div>
         <div class="input-field" id="input-field">
             <form method="get">
-                <input type="text" id="text-num" name="text-num" placeholder="    Pokémon name or id">
+                <input type="text" id="text-num" name="text-num" placeholder=" Pokémon name or id">
             </form>
         </div>
     </div>
